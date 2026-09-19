548c1e841858d3266dbf70866e7dbd3c8cfa760d
ANOLD671 <mwalearnold671@gmail.com>
2026-09-19 15:33:00 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ssh_vpn
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnToggle = new System.Windows.Forms.Button();
+            this.githubLink = new System.Windows.Forms.LinkLabel();
+            this.timer_check_status = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnGh = new System.Windows.Forms.Button();
+            this.btnTelegram = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+      
+            this.btnToggle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnToggle.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnToggle.Location = new System.Drawing.Point(18, 18);
+            this.btnToggle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnToggle.Name = "btnToggle";
+            this.btnToggle.Size = new System.Drawing.Size(390, 112);
+            this.btnToggle.TabIndex = 0;
+            this.btnToggle.Text = "Connect";
+            this.btnToggle.UseVisualStyleBackColor = true;
+            this.btnToggle.Click += new System.EventHandler(this.btnToggle_Click);
+           
+            this.githubLink.AutoSize = true;
+            this.githubLink.Location = new System.Drawing.Point(18, 394);
+            this.githubLink.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.githubLink.Name = "githubLink";
+            this.githubLink.Size = new System.Drawing.Size(97, 20);
+            this.githubLink.TabIndex = 2;
+            this.githubLink.TabStop = true;
+            this.githubLink.Text = "Github page";
+            this.githubLink.Visible = false;
+            
+            this.timer_check_status.Interval = 1000;
+            this.timer_check_status.Tick += new System.EventHandler(this.timer_check_status_Tick);
+            
+            this.notifyIcon1.BalloonTipText = "SSH VPN Disconected...";
+            this.notifyIcon1.Text = "SSH VPN NOTIF";
+            this.notifyIcon1.Visible = true;
+            
+          
+            this.lblStatus.BackColor = System.Drawing.Color.Red;
+            this.lblStatus.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F);
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblStatus.Location = new System.Drawing.Point(18, 140);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(390, 98);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Not Connected";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            
+            this.btnGh.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnGh.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnGh.BackgroundImage")));
+            this.btnGh.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnGh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGh.FlatAppearance.BorderSize = 0;
+            this.btnGh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGh.Location = new System.Drawing.Point(18, 248);
+            this.btnGh.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGh.Name = "btnGh";
+            this.btnGh.Size = new System.Drawing.Size(82, 80);
+            this.btnGh.TabIndex = 5;
+            this.btnGh.UseVisualStyleBackColor = false;
+            this.btnGh.Click += new System.EventHandler(this.btnGh_Click);
+            
+            
+            this.btnTelegram.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnTelegram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTelegram.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTelegram.FlatAppearance.BorderSize = 0;
+            this.btnTelegram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTelegram.Image = global::ssh_vpn.Properties.Resources.telegramIcon;
+            this.btnTelegram.Location = new System.Drawing.Point(118, 248);
+            this.btnTelegram.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnTelegram.Name = "btnTelegram";
+            this.btnTelegram.Size = new System.Drawing.Size(90, 80);
+            this.btnTelegram.TabIndex = 6;
+            this.btnTelegram.Text = "Telegram";
+            this.btnTelegram.UseVisualStyleBackColor = false;
+            this.btnTelegram.Click += new System.EventHandler(this.btnTelegram_Click);
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(424, 349);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGh);
+            this.Controls.Add(this.githubLink);
+            this.Controls.Add(this.btnToggle);
+            this.Controls.Add(this.btnTelegram);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SSH VPN";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.LinkLabel githubLink;
+        private System.Windows.Forms.Timer timer_check_status;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button btnGh;
+        private System.Windows.Forms.Button btnTelegram;
+        private System.Windows.Forms.Label lblStatus;
+        public System.Windows.Forms.Button btnToggle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Renci.SshNet;
+using System.Net.Http;
+
+
+namespace ssh_vpn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadConfigFromGitHub();
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
+        }
+
+        SshClient sshClient = new SshClient("0.0.0.0", 22, "0000", "0000");
+        ForwardedPortDynamic portForwarded = new ForwardedPortDynamic(9000);
+
+        
+        string ip = "";
+        string username = "";
+        string password = "";
+        int port = 22;
+
+
+        void Connect()
+        {
+            btnToggle.Text = "Connecting...";
+
+            
+            if (string.IsNullOrEmpty(ip) || string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || port == 0)
+            {
+                MessageBox.Show("Error : Failed to load VPN configuration from GitHub.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btnToggle.Text = "Connect";
+                return;
+            }
+
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback((state) =>
+            {
+                sshClient = new SshClient(ip, port, username, password);
+
+                try
+                {
+                    sshClient.Connect();
+                    sshClient.AddForwardedPort(portForwarded);
+                    portForwarded.Start();
+
+                    set_windows_proxy();
+
+                    Invoke((MethodInvoker)delegate
+                    {
+                        lblStatus.BackColor = Color.Green;
+                        lblStatus.Text = "Connected      00:00:00";
+                        btnToggle.Text = "Disconnect";
+
+                        timer_check_status.Enabled = true;
+                        timer_check_status.Start();
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error : " + ex.Message, "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Invoke((MethodInvoker)delegate { btnToggle.Text = "Connect"; });
+                }
+                finally
+                {
+                    Invoke((MethodInvoker)delegate { btnToggle.Enabled = true; });
+                }
+            }));
+        }
+
+        void Disconnect()
+        {
+            btnToggle.Text = "Disconnecting...";
+
+            portForwarded.Stop();
+            sshClient.Disconnect();
+            unset_windows_proxy();
+
+            btnToggle.Text = "Connect";
+            lblStatus.BackColor = Color.Red;
+            lblStatus.Text = "Not Connected";
+
+            timer_check_status.Enabled = false;
+            timer_check_status.Stop();
+            seconds = 0;
+
+            btnToggle.Enabled = true;
+        }
+
+
+        private void btnToggle_Click(object sender, EventArgs e)
+        {
+            btnToggle.Enabled = false;
+            Cursor.Current = Cursors.WaitCursor;
+
+            if (sshClient.IsConnected)
+                Disconnect();
+
+            else Connect();
+
+            Cursor.Current = Cursors.Default;
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!sshClient.IsConnected) return;
+
+            DialogResult result = MessageBox.Show("Do you really wish to exit? the connection will be stopped.", "Exit Program?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+                e.Cancel = true;
+            else
+                Disconnect();
+
+        }
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            if (sshClient.IsConnected)
+                btnToggle_Click(null, null);
+
+            Application.Exit();
+        }
+
+        
+        bool back_status = false;
+        int seconds = 0;
+        private void timer_check_status_Tick(object sender, EventArgs e)
+        {
+            if (!sshClient.IsConnected && sshClient.IsConnected != back_status)
+            {
+                Disconnect();
+
+                notifyIcon1.Icon = SystemIcons.Warning;
+                notifyIcon1.ShowBalloonTip(10);
+            }
+            else if (sshClient.IsConnected && sshClient.IsConnected != back_status)
+            {
+                seconds = 0;
+                lblStatus.Text = "Connected      00:00:00";
+            }
+            else if (sshClient.IsConnected)
+            { 
+                seconds++;
+
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int remainingSeconds = seconds % 60;
+                lblStatus.Text = "Connected      " + hours.ToString("D2") + ":" + minutes.ToString("D2") + ":" + remainingSeconds.ToString("D2");
+            }
+
+            back_status = sshClient.IsConnected;
+        }
+
+        private void set_windows_proxy()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyEnable", 1);
+            registry.SetValue("ProxyServer", "socks5://127.0.0.1:9000");
+
+            WinINetInterop.InternetSetOption(IntPtr.Zero, WinINetInterop.INTERNET_OPTION_SETTINGS_CHANGED, IntPtr.Zero, 0);
+            WinINetInterop.InternetSetOption(IntPtr.Zero, WinINetInterop.INTERNET_OPTION_REFRESH, IntPtr.Zero, 0);
+        }
+
+        private void unset_windows_proxy()
+        {
+            RegistryKey registry = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings", true);
+            registry.SetValue("ProxyEnable", 0);
+            registry.SetValue("ProxyServer", "");
+        }
+
+        private void LoadConfigFromGitHub()
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    string url = "https://raw.githubusercontent.com/anold20046/hog/refs/heads/main/vpn-config.txt";
+                    string result = client.GetStringAsync(url).Result;
+                   
+                    foreach (string line in result.Split('\n'))
+                    {
+                        if (line.Contains("="))
+                        {
+                            string[] parts = line.Split('=');
+                            if (parts.Length == 2)
+                            {
+                                string key = parts[0].Trim().ToUpper();
+                                string value = parts[1].Trim();
+                                switch (key)
+                                {
+                                    case "IP":
+                                        ip = value;
+                                        break;
+                                    case "PORT":
+                                        int.TryParse(value, out port);
+                                        break;
+                                    case "USERNAME":
+                                        username = value;
+                                        break;
+                                    case "PASSWORD":
+                                        password = value;
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Failed to load config: " + ex.Message);
+            }
+        }
+
+        private string registery_get_data(string name)
+        {
+            string keyName = "ssh_vpn";
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                if (key == null)
+                    return "";
+                else
+                    return key.GetValue(name) as string;
+            }
+        }
+
+        private void btnGh_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/ANOLD671/ssh-vpn.git");
+        }
+
+        private void btnTelegram_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://t.me/+wLrhr-mIdGwwYmM0");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+    public static class WinINetInterop
+    {
+        public const int INTERNET_OPTION_SETTINGS_CHANGED = 39;
+        public const int INTERNET_OPTION_REFRESH = 37;
+
+        [System.Runtime.InteropServices.DllImport("wininet.dll", SetLastError = true, CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern bool InternetSetOption(IntPtr hInternet, int dwOption, IntPtr lpBuffer, int lpdwBufferLength);
+    }
+}
